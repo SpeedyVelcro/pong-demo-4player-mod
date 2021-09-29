824f3126a4d499ea43508e78ee6cfac93ac2896f
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2021-09-29 17:00:51 +0100

    Ball resets to centre and random direction

--- logic/ball.gd
+++ logic/ball.gd
@@ -4,15 +4,33 @@ const DEFAULT_SPEED = 200
 
 var _speed = DEFAULT_SPEED
 var direction = Vector2.DOWN
+var moving = false
 
 onready var _initial_pos = position
 
 func _process(delta):
-	_speed += delta * 2
-	position += _speed * delta * direction
+	if Input.is_action_just_pressed("ui_select"):
+		moving = true
+	if moving:
+		_speed += delta * 2
+		position += _speed * delta * direction
 
 
 func reset():
-	direction = Vector2.LEFT
+	#var dir_choice = randi() % 4
+	#match dir_choice:
+	#	0:
+	#		direction = Vector2.LEFT
+	#	1:
+	#		direction = Vector2.UP
+	#	2:
+	#		direction = Vector2.RIGHT
+	#	3:
+	#		direction = Vector2.DOWN
+	direction = Vector2(0, 0)
+	while direction.length_squared() == 0:
+		direction.x = rand_range(-1, 1)
+		direction.y = rand_range(-1, 1)
+	direction = direction.normalized()
 	position = _initial_pos
 	_speed = DEFAULT_SPEED
